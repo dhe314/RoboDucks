8498562c88845e6e025fb762ae49a46ff0f8a39e
dhe314 <33498877+dhe314@users.noreply.github.com>
2018-09-15 23:49:10 -0500

    Update MotorClass.cpp

--- MotorClass.cpp
+++ MotorClass.cpp
@@ -1,40 +1,67 @@
 #include "MotorClass.h"
+#include "Arduino.h"
+
+MotorController::MotorController(){
+};
+
+//do you have to re-set the speed when the direction of the wheel's spin changes??
 
 
-MotorController()::MotorController(){
-}
-
-MotorController()::forward(){
+void MotorController::forward(){
 	digitalWrite(Rpin1, HIGH);
 	digitalWrite(Rpin2, LOW);
+  analogWrite(Rspeed, 30);
+  
 	digitalWrite(Lpin1, HIGH);
 	digitalWrite(Lpin2, LOW);
+  analogWrite(Lspeed, 30);
 }
 	
-MotorController()::back(){
+void MotorController::back(){
+  digitalWrite(Rpin1, LOW);
 	digitalWrite(Rpin2, HIGH);
-	digitalWrite(Rpin1, LOW);
+  analogWrite(Rspeed, 30);
+
+  digitalWrite(Lpin1, LOW);
 	digitalWrite(Lpin2, HIGH);
-	digitalWrite(Lpin1, LOW);
+	analogWrite(Lspeed, 30);
 }
 	
-MotorController()::left(){
+void MotorController::left(){
 	digitalWrite(Rpin1, HIGH);
 	digitalWrite(Rpin2, LOW);
-	
-	digitalWrite(Lpin2, LOW);
-	digitalWrite(Lpin1, HIGH);
-}
-	
-MotorController()::right(){
-	digitalWrite(wheel1pin1, LOW);
-	digitalWrite(wheel1pin2, HIGH);
-}
-	
-MotorController()::init(int fr, int fl, int br, int bl){
-	this->wheel1pin1 = fr;
-	this->wheel1pin2 = fl;
-	this->BACK_R = br;
-	this->BACK_L = bl;
+  analogWrite(Rspeed, 30);
 
+  digitalWrite(Lpin1, LOW);
+  digitalWrite(Lpin2, HIGH);
+  analogWrite(Lspeed, 30);
+}
+	
+void MotorController::right(){
+	digitalWrite(Rpin1, LOW);
+	digitalWrite(Rpin2, HIGH);
+  analogWrite(Rspeed, 30);
+
+  digitalWrite(Lpin1, HIGH);
+  digitalWrite(Lpin2, LOW);
+  analogWrite(Lspeed, 30);
+}
+
+void MotorController::stop(){
+  digitalWrite(Rpin1, LOW);
+  digitalWrite(Rpin2, LOW);
+  digitalWrite(Lpin1, LOW);
+  digitalWrite(Lpin2, LOW);
+
+  analogWrite(Rspeed, 0);
+  analogWrite(Lspeed, 0);
+}
+
+void MotorController::init(int r1, int l1, int r2, int l2, int rs, int ls){
+	this->Rpin1 = r1;
+	this->Rpin2 = r2;
+	this->Lpin1 = l1;
+	this->Lpin2 = l2;
+  this->Rspeed = rs;
+  this->Lspeed = ls;
 }

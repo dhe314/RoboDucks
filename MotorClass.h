1a920423b12e6bdaa4093f5c86d48c7e7976e531
dhe314 <33498877+dhe314@users.noreply.github.com>
2018-09-15 23:44:31 -0500

    Update MotorClass.h

--- MotorClass.h
+++ MotorClass.h
@@ -1,12 +1,25 @@
 #ifndef MOTOR_H
 #define MOTOR_H
+
 class MotorController{
 	public:
+    MotorController();
+    
+    void forward();
+    void back();
+    void right();
+    void left();
+    void stop();
+    void init(int r1, int l1, int r2, int l2, int rs, int ls);
+    
 	private:
+    int Rpin1;
+    int Rpin2;
+    int Lpin1;
+    int Lpin2;
+
+    int Rspeed;
+    int Lspeed;
 };
-
-
-
-
 
 #endif
